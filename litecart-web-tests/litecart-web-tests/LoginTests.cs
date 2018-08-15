5a41945357dd4cc9660f49a9a3eaef367bc66a42
Vasilisa Smirnova <lissarider@gmail.com>
2018-08-15 17:38:26 +0300

    Задание 3. Сделайте сценарий логина
    Реализвоан тестовый сценарий для логина в панель администрирования учебного приложения http://localhost/litecart/admin/.

---
+++ litecart-web-tests/litecart-web-tests/LoginTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace LitecartWebTests
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private string baseURL;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            baseURL = "http://localhost";
+        }
+
+        [Test]
+        public void LoginWithValidCredentials()
+        {
+            // Open admin login oage
+            driver.Navigate().GoToUrl(baseURL + "/litecart/admin/login.php");
+            // Enter username
+            driver.FindElement(By.Name("username")).Clear();
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            // Enter password
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            // Login
+            driver.FindElement(By.CssSelector("button[name=\"login\"]")).Click();
+            // Logout
+            driver.FindElement(By.CssSelector("i.fa.fa-sign-out.fa-lg")).Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+    }
+}
